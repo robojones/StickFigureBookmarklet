36bb8ac827ec5ebe8b48eefcdc41c1228e4af19d
Jonathan Scholz <contact@jonathan-scholz.de>
2025-05-01 22:23:52 -0400

    add error details to decoder

--- public/game.js
+++ public/game.js
@@ -162,7 +162,7 @@
 			try {
 				cb(null, JSON.parse(atob(BUNDLED_RESOURCES[name])));
 			} catch(e) {
-				cb(e);
+				cb(new Error(`Failed to decode Base64 string: ${name}: ${e.message}`);
 			}
 		},
 		'png': function (name, cb) {
